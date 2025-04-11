0e3e8e08bb1a430d9ec35be5edc9eb771b4acd45
Plikii <marioua672@gmail.com>
2025-04-11 19:32:13 +0300

    fix

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
 
   return (
     <section id="about-me" className="pt-32 " data-aos="fade-up">
-      <div className=" 2xl:-mb-10 flex w-full items-center justify-center pb-10">
+      <div className=" 2xl:-mb-9 flex w-full items-center justify-center pb-10">
         <h2 className="w-36 rounded-2xl bg-card py-2 text-center font-bold text-lg text-primary ">
           {t("category")}
         </h2>
@@ -20,7 +20,7 @@ export default function AboutMe() {
             width={420}
             height={420}
             priority
-            className=" h-[360] rounded-full object-contain"
+            className=" h-[360] rounded-b-full object-contain"
           />
         </div>
         <div className=" flex flex-col space-y-6 lg:items-start ">
