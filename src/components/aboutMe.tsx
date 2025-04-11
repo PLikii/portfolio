a16dda63866d117ff5fb37a9734218e41aad50a7
Plikii <marioua672@gmail.com>
2025-04-11 18:55:24 +0300

    fix

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -5,41 +5,42 @@ export default function AboutMe() {
   const t = useTranslations("aboutMe");
 
   return (
-    <section
-      id="about-me"
-      className="flex flex-col items-center justify-around gap-6 pt-20 2xl:flex-row"
-    >
-      <div className=" pointer-events-none select-none" data-aos="fade-up">
-        <Image
-          src="/myImage.png"
-          alt={t("title")}
-          width={420}
-          height={420}
-          priority
-          className=" h-[420] w-full rounded-full object-contain "
-        />
-      </div>
-      <div
-        className=" flex flex-col items-center space-y-6 lg:items-start "
-        data-aos="fade-down"
-      >
-        <h2 className=" w-36 rounded-2xl bg-card py-2 text-center font-bold text-lg text-primary">
+    <section id="about-me" className="pt-32 ">
+      <div className=" flex w-full items-center justify-center">
+        <h2 className=" -mb-10 w-36 rounded-2xl bg-card py-2 text-center font-bold text-lg text-primary">
           {t("category")}
         </h2>
+      </div>
 
-        <h1 className=" font-extrabold text-2xl text-title sm:text-4xl">
-          {t("title")}
-        </h1>
-
-        <div className=" sm:text-lg">{t("description1")}</div>
-
-        <div className=" space-y-3 sm:text-lg">
-          <p>{t("description2")}</p>
-          <p>{t("description3")}</p>
-          <p>{t("description4")}</p>
+      <div className="flex flex-col items-center justify-around gap-6 2xl:flex-row">
+        <div className=" pointer-events-none select-none" data-aos="fade-up">
+          <Image
+            src="/myImage.png"
+            alt={t("title")}
+            width={420}
+            height={420}
+            priority
+            className=" h-[420] w-full rounded-full object-contain "
+          />
         </div>
+        <div
+          className=" flex flex-col items-center space-y-6 lg:items-start "
+          data-aos="fade-down"
+        >
+          <h1 className=" font-extrabold text-2xl text-title sm:text-4xl ">
+            {t("title")}
+          </h1>
 
-        <div className=" sm:text-lg">{t("description5")}</div>
+          <div className=" sm:text-lg">{t("description1")}</div>
+
+          <div className=" space-y-3 sm:text-lg">
+            <p>{t("description2")}</p>
+            <p>{t("description3")}</p>
+            <p>{t("description4")}</p>
+          </div>
+
+          <div className=" sm:text-lg">{t("description5")}</div>
+        </div>
       </div>
     </section>
   );
