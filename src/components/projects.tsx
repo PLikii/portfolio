971571b4404a8ed32a78205a555a2b13a8a130e3
Plikii <marioua672@gmail.com>
2025-05-20 22:51:45 +0300

    add progect fix text

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,21 +1,22 @@
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+import { FaGithub } from "react-icons/fa";
 
 export default function Projects() {
   const t = useTranslations("projects");
 
   const tech = title => (
     <em className=" mt-4 mr-4 inline-block rounded-2xl bg-card px-4 py-2 text-sm duration-200 hover:scale-105 hover:bg-primary">
-      {title}{" "}
+      {title}
     </em>
   );
 
-  const project = (image, href, title, description, techs, techsSize) => (
+  const project = (image, href, title, description, git, techs, techsSize) => (
     <div
-      className=" rounded-2xl border border-border duration-500 lg:hover:scale-105"
-      data-aos="zoom-out-up"
+      className=" rounded-2xl border border-border duration-600 "
+      data-aos="zoom-in-up"
     >
-      <a href={href} className=" cursor-pointer" data-aos="zoom-out-up">
+      <a href={href} className=" cursor-pointer">
         <Image
           src={image}
           alt="progectImage"
@@ -24,19 +25,26 @@ export default function Projects() {
           priority
           className="pointer-events-none w-full rounded-t-2xl object-contain"
         />
-        <div className=" space-y-4 p-4 ">
-          <h3 className="text-title text-xl">{title}</h3>
-          <p>{description}</p>
-          <div>
-            <h4 className=" text-lg text-title">{t("techStack")}</h4>
-            <p>
-              {Array.from({ length: techsSize }).map((_, i) => {
-                return tech(t(`project.${techs}.techs.${i}`));
-              })}
-            </p>
-          </div>
-        </div>
       </a>
+      <div className=" space-y-4 p-4 ">
+        <div className=" flex items-center justify-between gap-4">
+          <h3 className="overflow-hidden truncate whitespace-nowrap text-title text-xl">
+            {title}{" "}
+          </h3>
+          <a href={git} className="pr-4">
+            <FaGithub className=" size-8 duration-300 hover:scale-105 hover:fill-primary" />
+          </a>
+        </div>
+        <p>{description}</p>
+        <div>
+          <h4 className=" text-lg text-title">{t("techStack")}</h4>
+          <p>
+            {Array.from({ length: techsSize }).map((_, i) => {
+              return tech(t(`project.${techs}.techs.${i}`));
+            })}
+          </p>
+        </div>
+      </div>
     </div>
   );
 
@@ -60,12 +68,13 @@ export default function Projects() {
       </div>
 
       <div className=" grid grid-cols-1 gap-10 lg:grid-cols-2">
-        {Array.from({ length: 2 }).map((_, i) => {
+        {Array.from({ length: 4 }).map((_, i) => {
           return project(
             t(`project.${i}.image`),
             t(`project.${i}.href`),
             t(`project.${i}.title`),
             t(`project.${i}.description`),
+            t(`project.${i}.git`),
             i,
             t(`project.${i}.techsSize`),
           );
